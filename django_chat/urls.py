f3d2685f57c72289da6113919b6d84a649435703
liaozhiming <liaozhimingandy@qq.com>
2024-05-15 22:37:38 +0800

    perf(core): 重构
    1.部分代码重构

--- django_chat/urls.py
+++ django_chat/urls.py
@@ -29,7 +29,6 @@ urlpatterns = [
     path('', api.urls)
 ]
 
-# 开发环境提供静态文件和多媒体查看功能
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# 开发环境提供静态文件和多媒体查看功能;这一般会在 DEBUG is set to True 情况下由 runserver 自动完成
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
